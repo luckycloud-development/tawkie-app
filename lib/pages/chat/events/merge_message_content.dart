b9816014e0edb35f7a3998dadb61b36d11714965
Vincent Paulin <ecniv81@gmail.com>
2024-01-29 10:45:23 +0100

    feat: redirect button to the post

--- lib/pages/chat/events/merge_message_content.dart
+++ lib/pages/chat/events/merge_message_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
 import 'package:tawkie/pages/chat/events/message_content.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MergeMessageContent extends StatelessWidget {
   final List<Event> events;
@@ -9,18 +10,52 @@ class MergeMessageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Logic to display events in the group
-    // To display each event individually via MessageContent.
+    // Ignore the first and last events in the list
+    final List<Event> eventsToDisplay =
+        events.sublist(1, events.length - 1).reversed.toList();
+    final Event firstEvent = events.first;
+    final Uri url =
+        Uri.parse(firstEvent.text); // Uri.parse to convert the string to Uri
+
     return Column(
-      children: events
-          .map((event) => MessageContent(
-                event,
-                textColor: Colors.blue,
-                borderRadius: BorderRadius.zero,
-              ))
-          .toList()
-          .reversed
-          .toList(),
+      children: [
+        // Display the remaining events in the group
+        ...eventsToDisplay.map(
+          (event) => MessageContent(
+            event,
+            textColor: Colors.blue,
+            borderRadius: BorderRadius.zero,
+          ),
+        ),
+        // Display the first event as a button with the extracted link
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () {
+              // Handle button click
+              openUrl(url);
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(8.0),
+                  bottomRight: Radius.circular(8.0),
+                ),
+              ),
+            ),
+            child: Text('Go to post'),
+          ),
+        ),
+      ],
     );
+  }
+
+  // Function to open URL
+  void openUrl(Uri url) async {
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
