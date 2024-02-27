95bdb484472757c455bfd22d481841830b8a09b2
Vincent Paulin <ecniv81@gmail.com>
2024-02-27 10:47:03 +0100

    feat: web view to connect and extract token

--- lib/pages/add_bridge/web_view_connection.dart
+++ lib/pages/add_bridge/web_view_connection.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:hive/hive.dart';
+import 'package:http/http.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tawkie/main.dart';
+import 'package:tawkie/utils/client_manager.dart';
 import 'package:tawkie/widgets/future_loading_dialog_custom.dart';
 import 'package:tawkie/widgets/notifier_state.dart';
 import 'package:webview_cookie_manager/webview_cookie_manager.dart';
@@ -26,6 +32,12 @@ class WebViewConnection extends StatefulWidget {
 class _WebViewConnectionState extends State<WebViewConnection> {
   InAppWebViewController? _webViewController;
   final cookieManager = WebviewCookieManager();
+  String? authToken;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,27 +49,42 @@ class _WebViewConnectionState extends State<WebViewConnection> {
         initialUrlRequest: URLRequest(
           url: WebUri(widget.network.urlLogin!),
         ),
-        onWebViewCreated: (InAppWebViewController controller) {
+        onWebViewCreated: (InAppWebViewController controller) async {
           _webViewController = controller;
+
+          // Cache reset
+          await _webViewController?.clearCache();
         },
-        onLoadStop: (InAppWebViewController controller, Uri? url) async {
-          // Check the URL when the page finishes loading
+        onReceivedHttpError: (InAppWebViewController controller,
+            WebResourceRequest request, WebResourceResponse response) async {
+          if (widget.network.name == 'Discord') {
+            String result =
+                ""; // Variable to store the result of the connection
 
-          String result = ""; // Variable to store the result of the connection
+            String headers = request.headers.toString();
+            RegExp regExp = RegExp(r'Authorization: ([^,]+)');
+            Match? match = regExp.firstMatch(headers);
 
-          switch (widget.network.name) {
-            case "Linkedin":
-              if (url != null &&
-                  url.toString().contains(widget.network.urlRedirect!)) {
-                // await showCustomLoadingDialog(
-                //   context: context,
-                //   future: () async {
-                //     result = await widget.botBridgeConnection
-                //         .createBridgeLinkedin(context, cookieManager,
-                //             connectionState, widget.network);
-                //   },
-                // );
-              }
+            String? authorizationHeaderValue;
+
+            if (match != null && match.groupCount > 0) {
+              authorizationHeaderValue = match.group(1)!;
+              print('Authorization header value: $authorizationHeaderValue');
+            }
+
+            if (authorizationHeaderValue != null) {
+              await showCustomLoadingDialog(
+                context: context,
+                future: () async {
+                  result = await widget.botBridgeConnection.createBridgeDiscord(
+                      context,
+                      cookieManager,
+                      connectionState,
+                      widget.network,
+                      authorizationHeaderValue!);
+                },
+              );
+
               if (result == "success") {
                 // Close the current page
                 Navigator.pop(context);
@@ -70,8 +97,7 @@ class _WebViewConnectionState extends State<WebViewConnection> {
                 // Call callback function with success result
                 widget.onConnectionResult(true);
               }
-              break;
-            // Other network
+            }
           }
         },
       ),
