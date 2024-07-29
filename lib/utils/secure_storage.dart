ef7c5b6237565d9f1f438dd5df6f1a6685284c03
Vincent Paulin <ecniv81@gmail.com>
2024-07-29 11:11:50 +0200

    feat: secure storage utils

---
+++ lib/utils/secure_storage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageUtil {
+  static const FlutterSecureStorage _secureStorage = FlutterSecureStorage();
+
+  static Future<void> storeUserCreatedIndicator() async {
+    await _secureStorage.write(key: 'userCreated', value: 'true');
+  }
+
+  static Future<void> clearUserCreatedIndicator() async {
+    await _secureStorage.delete(key: 'userCreated');
+  }
+
+  static Future<String?> checkUserCreatedIndicator() async {
+    return await _secureStorage.read(key: 'userCreated');
+  }
+
+  static Future<void> storeSessionToken(String? sessionToken) async {
+    if (sessionToken != null) {
+      await _secureStorage.write(key: 'sessionToken', value: sessionToken);
+    }
+  }
+
+  static Future<String?> getSessionToken() async {
+    return await _secureStorage.read(key: 'sessionToken');
+  }
+}
