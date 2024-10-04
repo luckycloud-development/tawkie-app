6e46bc7d973ec9328a6789d75fe3873fc633edcc
Vincent Paulin <ecniv81@gmail.com>
2024-10-04 09:43:45 +0200

    debug update_service

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -10,7 +10,7 @@ Future<String?> getLatestVersionFromGitHub() async {
     final data = jsonDecode(response.body);
     final latestVersion = data['tag_name']; // Retrieves the tag for the latest version
 
-    print("La dernière version recup de github: $latestVersion");
+    print("La dernière version récupérée de GitHub : $latestVersion");
     return latestVersion;
   } else {
     print('Erreur lors de la récupération des informations de version : ${response.statusCode}');
@@ -19,22 +19,29 @@ Future<String?> getLatestVersionFromGitHub() async {
 }
 
 Future<String?> getWindowsExeDownloadUrl() async {
-  final String url = "https://api.github.com/repos/Tawkie/tawkie-app/releases/latest";
-  final response = await http.get(Uri.parse(url));
+  try {
+    final String url = "https://api.github.com/repos/Tawkie/tawkie-app/releases/latest";
+    final response = await http.get(Uri.parse(url));
 
-  if (response.statusCode == 200) {
-    final data = jsonDecode(response.body);
-    final List<dynamic> assets = data['assets'];
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final List<dynamic> assets = data['assets'];
 
-    // Parcours de la liste des assets pour trouver l'URL du fichier Windows (.exe)
-    for (var asset in assets) {
-      if (asset['name'].endsWith('.exe')) {  // Vérifie si le nom du fichier se termine par .exe
-        return asset['browser_download_url']; // Retourne l'URL de téléchargement
+      // Browse asset list to find URL of Windows file (.exe)
+      for (var asset in assets) {
+        if (asset['name'].endsWith('.exe')) {
+          print("URL du fichier .exe : ${asset['browser_download_url']}");
+          return asset['browser_download_url'];
+        }
       }
+      print('Aucun fichier .exe trouvé dans les assets de la release.');
+      return null;
+    } else {
+      print('Erreur lors de la récupération des informations de la release : ${response.statusCode}');
+      return null;
     }
-    print('Aucun fichier .exe trouvé dans les assets de la release.');
-  } else {
-    print('Erreur lors de la récupération des informations de la release : ${response.statusCode}');
+  } catch (e) {
+    print("Erreur lors de la récupération de l'URL : $e");
+    return null;
   }
-  return null;
 }
